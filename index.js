5ac02476817d5af44717480b0986760019d72753
Saloniaga <sagarwal9475@gmail.com>
2022-12-22 12:58:13 +0530

    created webhook api

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const bp = require("body-parser");
+const axios = require("axios");
 
 const app = express().use(bp.json());
+const token = "NOTOKEN"; //for sending request
+let mytoken = "strongtoken"; //for verifying webhook
 
 app.get("/", (req, res) => {
   res.send("hey");
@@ -12,12 +15,48 @@ app.get("/webhook", (req, res) => {
   let challenge = req.query["hub.challenge"];
   let token = req.query["hub.verify_token"];
 
-  let mytoken = "strongtoken";
   if (mode && token) {
     if (mode === "subscribe" && token === mytoken) {
       res.status(200).send(challenge);
     } else {
       res.status(403);
+    }
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  let body = req.body;
+  console.log(JSON.stringify(body, null, 2));
+
+  if (body.object) {
+    if (
+      body.entry &&
+      body.entry[0].changes &&
+      body.entry[0].changes[0].value.message &&
+      body.entry[0].changes[0].value.message[0]
+    ) {
+      let phone_no = body.entry[0].challenge[0].value.metadata.phone_number_id;
+      let from = body.entry[0].changes[0].value.messages[0].from;
+      let msg_body = body.entry[0].changes[0].value.messages[0].text.body;
+
+      axios({
+        method: "POST",
+        url:
+          "https://graph.facebook.com/v15.0/" +
+          phone_number_id +
+          "/message?access_token=" +
+          token,
+        data: {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "message from webhook" },
+        },
+        headers: { "Content-Type": "application/json" },
+      });
+
+      res.sendStatus(200);
+    } else {
+      res.sendStatus(404);
     }
   }
 });
