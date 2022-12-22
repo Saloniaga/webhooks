95c17a9573582279a6a10b6601222e7a3421ecd6
Saloniaga <sagarwal9475@gmail.com>
2022-12-22 12:17:54 +0530

    setting webhook api

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ app.get("/", (req, res) => {
   res.send("hey");
 });
 
+app.get("/webhook", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let challenge = req.query["hub.challenge"];
+  let token = req.query["hub.verify_token"];
+
+  let mytoken = "strongtoken";
+  if (mode && token) {
+    if (mode === "subscribe" && token === mytoken) {
+      res.status(200).send(challenge);
+    } else {
+      res.status(403);
+    }
+  }
+});
+
 app.listen(8000, () => {
   console.log("webhook is listening");
 });
